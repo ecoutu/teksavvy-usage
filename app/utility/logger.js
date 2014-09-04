dfa751f935bb8891298aab2af530b0cdcae985e7
Eric Coutu <eric.coutu@gmail.com>
2014-09-04 19:36:40 -0400

    show source in logs when NODE_ENV !== production

--- app/utility/logger.js
+++ app/utility/logger.js
@@ -5,7 +5,7 @@ var bsyslog = require('bunyan-syslog');
 module.exports.createLogger = function(name) {
   return bunyan.createLogger({
     name: name,
-    src: process.env.NODE_ENV === 'production',
+    src: process.env.NODE_ENV !== 'production',
     streams: [
       {
         level: 'debug',
